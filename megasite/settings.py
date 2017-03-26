9c05b98d278494d86138be4e388db6bfd0c2f210
Sagar Giri <sgiri@hctech.com>
2017-03-25 22:41:32 -0400

    added settings

--- megasite/settings.py
+++ megasite/settings.py
@@ -144,17 +144,17 @@ FILE_UPLOAD_PERMISSIONS = 644
 DATABASES = {
     "default": {
         # Add "postgresql_psycopg2", "mysql", "sqlite3" or "oracle".
-        "ENGINE": "django.db.backends.mysql",
+        "ENGINE": "django.db.backends.sqlite3",
         # DB name or path to database file if using sqlite3.
         "NAME": "megasite",
         # Not used with sqlite3.
-        "USER": "megasite_user",
+#        "USER": "megasite_user",
         # Not used with sqlite3.
-        "PASSWORD": "megasite_pass",
+#        "PASSWORD": "megasite_pass",
         # Set to empty string for localhost. Not used with sqlite3.
-        "HOST": "/cloudsql/sgiri-156214:us-central1:megasite",
+#        "HOST": "/cloudsql/sgiri-156214:us-central1:megasite",
         # Set to empty string for default. Not used with sqlite3.
-        "PORT": "3306",
+#        "PORT": "3306",
     }
 }
 
