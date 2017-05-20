9fb58e10ad15098b1b3d9c78a0bef69fceb61757
Sagar Giri <sgiri@hctech.com>
2017-05-20 11:27:18 -0400

    little change

--- megasite/settings.py
+++ megasite/settings.py
@@ -189,7 +189,7 @@ CACHE_MIDDLEWARE_KEY_PREFIX = PROJECT_APP
 # URL that handles the media served from MEDIA_ROOT. Make sure to use a
 # trailing slash.
 # Examples: "http://media.lawrence.com/media/", "http://example.com/media/"
-MEDIA_URL = "http://sgiri.com/media/"
+MEDIA_URL = "/media/"
 
 # MEDIA_URL = STATIC_URL + "media/"
 
